e0323c49c1a5004e919f936af6e1788a9f37b0f0
EC2 Default User <ec2-user@ip-172-31-18-146.ec2.internal>
2020-08-27 14:00:11 +0000

    added user input to cohort variable with default value

--- directory.rb
+++ directory.rb
@@ -1,30 +1,35 @@
-#let's put all students into an array
-students = [
-  [name: "Dr. Hannibal Lecter", cohort: :november],
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    puts "Which cohort are they in?"
+    cohort = gets.chomp.to_sym
+    months = [:Janurary, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+    if !months.include? cohort
+      cohort = :unknown
+    end
+    students << {name: name, cohort: cohort, place_of_birth: :UK}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) born in #{student[:place_of_birth]}".center(60)
   end
 end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
